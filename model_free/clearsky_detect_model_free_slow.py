f1be4524c3a171b830f3400d3904dd5669ad5cd2
bhellis725 <ellis.bh89@gmail.com>
2017-05-25 15:41:45 -0700

    refactored model-free detection to give significant performance boost.  old (slow) functions are now in clearsky_detect_model_free_slow.py file.  working on a method improve performance on days which are cloudy for extended periods of time (seen in SRRL BMS data - model_free_snl_srrl_comparison.ipynb).

--- model_free/clearsky_detect_model_free_slow.py
+++ model_free/clearsky_detect_model_free_slow.py
@@ -15,9 +15,55 @@ def main():
     data = data[(data.index >= '2016-07-01 12:00:00') & (data.index < '2016-07-02')]
     data = pd.Series(data['Global_Wm2'], index=data.index)
 
+    # ts_derivative(data)
     clear_times = model_free_detect(data)
-    clear_times = model_free_detect_meanval(data)
-    clear_times = model_free_detect_democratic(data)
+    print('----------')
+    clear_times = model_free_detect_fast(data)
+    # clear_times = model_free_detect_democratic(data)
+    # clear_times = model_free_detect_meanval(data)
+
+
+def model_free_detect_fast(data, window=30, metric_tol=.05, verbose=False):
+    '''Determine clear sky periods based on irradiance measurements.
+
+    Arguments
+    ---------
+    data: pd.Series
+        time series irradiance data
+    window: int
+        number of samples to include in each window
+    metric_tol: float
+        tolerance for determining clear skies
+    verbose: bool
+        whether or not to return components used to determine is_clear
+
+    Returns
+    -------
+    is_clear: pd.Series
+        boolean time series of clear times
+    components: dict, optional
+        contains series of normalized lengths, local integrals, and calculated metric
+    '''
+    # data quality check
+    if len(pd.unique(data.index.to_series().diff().dropna())) != 1:
+        raise NotImplementedError('You must use evenly spaced time series data.')
+    if data.isnull().values.any():
+        raise ValueError('NaN/infinity in data.  Process these first.')
+
+    is_clear = pd.Series(False, data.index)
+
+    components = calc_distances_integrals_fast(data, window)
+    distances = components['local_distances']
+    integrals = components['local_integrals']
+    metric = pd.Series(np.log(distances) / np.log(integrals), index=data.index)
+
+    is_clear = (metric <= metric_tol) & (data > 0.0)
+
+    if verbose:
+        components['metric'] = metric
+        return is_clear, components
+    else:
+        return is_clear
 
 
 def model_free_detect(data, window=30, metric_tol=.05, verbose=False):
@@ -96,15 +142,21 @@ def model_free_detect_democratic(data, window=30, metric_tol=.05, vote_pct=0.75,
     components = calc_distances_integrals(data, window)
     distances = components['local_distances']
     integrals = components['local_integrals']
-    metric = pd.Series(np.log(distances.values) / np.log(integrals.values), index=data.index)
+    metric = pd.Series(np.log(distances) / np.log(integrals), index=data.index)
 
-    H = la.hankel(np.arange(0, len(data) - window),
-                  np.arange(len(data) - window, len(data)))
+    for i in np.arange(0, len(metric) - window):
+        group = metric.iloc[i: i + window]
 
-    midpoints = np.apply_along_axis(get_midval, 1, H)
-    pcts = np.apply_along_axis(calc_pct, 1, metric.values[H], metric_tol)
 
-    is_clear.iloc[midpoints] = (pcts >= vote_pct) & (data[midpoints] > 0.0)
+        midpoint = group.iloc[[len(group) // 2 + 1]].index
+
+        bools = (group <= metric_tol).astype(int)
+        pct_pass = np.sum(bools) / window
+
+        if pct_pass >= vote_pct and data[midpoint].values > 0.0:
+                is_clear[midpoint] = True
+
+    # is_clear[data <= 0.0] = False
 
     if verbose:
         components['metric'] = metric
@@ -144,15 +196,18 @@ def model_free_detect_meanval(data, window=30, metric_tol=.05, verbose=False):
     components = calc_distances_integrals(data, window)
     distances = components['local_distances']
     integrals = components['local_integrals']
-    metric = pd.Series(np.log(distances.values) / np.log(integrals.values), index=data.index)
+    metric = pd.Series(np.log(distances) / np.log(integrals), index=data.index)
 
-    H = la.hankel(np.arange(0, len(data) - window),
-                  np.arange(len(data) - window, len(data)))
+    for i in np.arange(0, len(metric) - window):
+        group = metric.iloc[i: i + window]
+        midpoint = group.iloc[[len(group) // 2 + 1]].index
 
-    midpoints = np.apply_along_axis(get_midval, 1, H)
-    means = np.apply_along_axis(np.mean, 1, metric.values[H])
+        meanval = np.mean(group)
 
-    is_clear.iloc[midpoints] = (means <= metric_tol) & (data[midpoints] > 0.0)
+        if meanval <= metric_tol and data[midpoint].values > 0.0:
+            is_clear[midpoint] = True
+
+    # is_clear[data <= 0.0] = False
 
     if verbose:
         components['metric'] = metric
@@ -161,8 +216,49 @@ def model_free_detect_meanval(data, window=30, metric_tol=.05, verbose=False):
         return is_clear
 
 
-def calc_distances_integrals(data, window):
-    '''Calculate normalized distances and integrals of moving window.  Values
+def calc_distances_integrals(data, window=30):
+    '''Calculate distances of line and integrals of moving window.  Values
+    are reported at the central index of the window.
+
+    Arguments
+    ---------
+    data: pd.Series
+        time series irradiance data
+    window: int
+        number of samples to include in each window
+
+    Returns
+    -------
+    result: dict
+        time series data frame with distances and integrals
+    '''
+    if len(pd.unique(data.index.to_series().diff().dropna())) != 1:
+        raise NotImplementedError('You must use evenly spaced time series data.')
+    if data.isnull().values.any():
+        raise ValueError('NaN/infinity in data.  Process these first.')
+
+    local_distances = pd.Series(np.nan, index=data.index, name='local_distances')
+    local_integrals = pd.Series(np.nan, index=data.index, name='local_integrals')
+
+    for i in np.arange(0, len(data) - window):
+        group = data.iloc[i: i + window]
+        midpoint = group.iloc[[len(group) // 2 + 1]].index
+
+        d_total = ts_distance(group)
+        d_min = ts_distance(group.iloc[[0, -1]])
+        d_norm = d_total / d_min
+        integral = ts_integral(group)
+
+        local_distances[midpoint] = d_norm
+        local_integrals[midpoint] = integral
+
+    result = {'local_distances': local_distances,
+              'local_integrals': local_integrals}
+
+    return result
+
+def calc_distances_integrals_fast(data, window=30):
+    '''Calculate distances of line and integrals of moving window.  Values
     are reported at the central index of the window.
 
     Arguments
@@ -186,9 +282,9 @@ def calc_distances_integrals(data, window):
     local_integrals = pd.Series(np.nan, index=data.index, name='local_integrals')
 
     H = la.hankel(np.arange(0, len(data) - window),
-                  np.arange(len(data) - window, len(data)))
+                            np.arange(len(data) - window, len(data)))
 
-    midpoints = np.apply_along_axis(get_midval, 1, H)
+    midpoints = np.apply_along_axis(calc_midpoint, 1, H)
     distances = np.apply_along_axis(calc_distance, 1, data.values[H])
     integrals = np.apply_along_axis(calc_integral, 1, data.values[H])
 
@@ -200,82 +296,61 @@ def calc_distances_integrals(data, window):
 
     return result
 
-
-def get_midval(array):
-    '''Returns element at the midpoint of an array.
-
-    Arguments
-    ---------
-    array: numpy array
-
-    Returns
-    -------
-    midval: element at midpoint of array
-        type depends on array elements
-    '''
+def calc_midpoint(array):
     midpoint = (len(array) // 2) + 1
-    midval = array[midpoint]
-    return midval
-
+    return array[midpoint]
 
 def calc_distance(array):
-    '''Calculate normalized distance of array.
-
-    Normalized distance is sum of the distsances of line segments divided
-    by the distance of the line segment of the end points.  The distance between
-    points in the x direction is assumed to be one.
-
-    Arguments
-    ---------
-    array: numpy array
-
-    Returns
-    -------
-    d_norm: float
-        normalized distance
-    '''
     d_value = np.diff(array)
     d_total = np.sum(np.sqrt(np.square(d_value[:]) + 1))
     d_line  = np.sqrt(np.square(array[-1] - array[0]) + np.square(len(array) - 1))
-    d_norm = d_total / d_line
-    return d_norm
-
+    return d_total / d_line
 
 def calc_integral(array):
-    '''Calculate integral of array values.
+    return np.trapz(array)
 
-    Array values are assumed to be y values and dx is assumed to be one.
+def ts_distance(series):
+    '''Calculate distance of a line travels.
 
-    Arguments
-    ---------
-    array: numpy array
+    Aruments
+    --------
+    series: pd.Series
+        time series of data
+
+    Returns
+    -------
+    distance: float
+        distance of line
+    '''
+    d_index = series.index.to_series().diff().dt.seconds.div(60, fill_value=0).astype(int).values
+    d_value = series.diff().values
+    distance = np.sum(np.sqrt(np.square(d_index[1:]) + np.square(d_value[1:])))
+    return distance
+
+def ts_derivative(series):
+    # d_index = series.index.to_series().diff().dt.seconds.div(60, fill_value=0).astype(int).values
+    # d_value = series.diff().values
+    # derivatives = d_value / d_index
+    gradient = np.gradient(series.values)
+    return gradient
+
+
+
+def ts_integral(series):
+    '''Calculate integral of data points (1-dimension).
+
+    Aruments
+    --------
+    series: pd.Series
+        time series of data
 
     Returns
     -------
     val: float
-        integral of array
-
+        integral value
     '''
-    val = np.trapz(array)
+    val = np.trapz(series.values.ravel())
     return val
-
-
-def calc_pct(array, metric_tol):
-    '''Calculate percent of array elements that are less than or equal to a tolerance.
-
-    Arguments
-    ---------
-    array: numpy array
-    metric_tol: numeric
-        tolerance to be less than/equal to
-
-    Returns
-    -------
-    pct: float
-        percent of values that are <= metric_tol
-    '''
-    pct = np.sum((array <= metric_tol).astype(int)) / len(array)
-    return pct
 
 
 if __name__ == '__main__':
